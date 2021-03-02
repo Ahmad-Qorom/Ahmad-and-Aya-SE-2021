6318838a55e0bd971fba8a20639d00b872aaf871
Ahmad-Qorom <77454155+Ahmad-Qorom@users.noreply.github.com>
2021-03-02 15:09:22 +0200

    feature 1 and 3 and thire codeing

---
+++ OurProject/src/main/java/library_project/productionCode.java
@@ -0,0 +1,51 @@
+package library_project;
+import java.util.ArrayList;
+
+public class productionCode
+{
+	private ArrayList<insertToList> library=new ArrayList<insertToList>();
+	private insertToList obj;
+	private insertToList books;
+
+	
+	public productionCode(insertToList obj)
+	{
+		this.obj = obj;
+	}
+	
+	
+	public boolean iscorrect(boolean password)
+    {
+   	 if(password)
+   		 return true;
+   	 
+   	 else 
+   		 return false;
+    }
+
+	
+	
+	public boolean addBook(String bookTitle,String bookAuthor,String bookISBN,String bookSignature,boolean done)
+	{
+		books=new insertToList();
+	    books.addBook(bookTitle, bookAuthor, bookISBN, bookSignature);
+	    library.add(books);
+	    
+	    if(books.ISBN==null && library.size()==0)
+	        return false;
+	    else 
+	    	return true;
+	}
+	
+	/*public void printBook()
+	{
+		System.out.println("number of books in library = "+library.size());
+		obj=library.get(0);
+		
+		
+		
+		System.out.println("Book Name     : "+obj.Title);
+		System.out.println("Book ISBN     : "+obj.ISBN);
+		System.out.println("Book Signature: "+obj.Signature);
+	}*/
+}
---
+++ OurProject/src/test/java/test_libraryProject/configLibrary.java
@@ -0,0 +1,13 @@
+package test_libraryProject;
+import org.junit.runner.RunWith;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)       
+@CucumberOptions(plugin="html: projectTestingReport/test_Library.html",features="use_cases",glue="test_libraryProject") 
+public class configLibrary
+{
+
+}
