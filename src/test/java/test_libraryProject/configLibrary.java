e5f1fda0edcf1bce4ed535bd5c38d10f34f6d86f
AyaGhazal <78089393+AyaGhazal@users.noreply.github.com>
2021-03-09 16:46:11 +0200

    update 3

--- src/test/java/test_libraryProject/configLibrary.java
+++ src/test/java/test_libraryProject/configLibrary.java
@@ -1,5 +1,18 @@
 package test_libraryProject;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith (Cucumber.class)
+@CucumberOptions(plugin= "html: html/cucumber.html", features="use_cases",glue= {"test_libraryProject"}
+
+)
+
+
 public class configLibrary {
+	
 
 }
